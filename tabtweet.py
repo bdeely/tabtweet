4af7ad1dd432c1e1860b00007e1738563804addd
bdeely <brian.s.deely@gmail.com>
2013-11-26 11:41:49 -0800

    Tabtweet code
    Twitter API and Tableau extract API in python. Creates a Tableau data extract, .tde, to bring into Tableau for analysis.

---
+++ tabtweet.py
@@ -0,0 +1,93 @@
+from TwitterAPI import TwitterAPI
+import time
+import dataextract as tde
+import os
+
+#All of this is for creating the Tableau extract
+tdename = 'tableautwitter.tde'
+try:
+    tdefile = tde.Extract(tdename)
+except:
+    os.remove(tdename)
+    tdefile = tde.Extract(tdename)
+
+fields = ['created_at', 'id', 'text', 'source', 'lang', 'place']
+    
+#Create the tableDef
+tableDef = tde.TableDefinition()
+tableDef.addColumn('user', tde.Type.UNICODE_STRING)
+tableDef.addColumn('created_at', tde.Type.UNICODE_STRING)
+tableDef.addColumn('id', tde.Type.CHAR_STRING)
+tableDef.addColumn('text', tde.Type.UNICODE_STRING)
+tableDef.addColumn('lang', tde.Type.UNICODE_STRING)
+tableDef.addColumn('country_name', tde.Type.UNICODE_STRING)
+tableDef.addColumn('longitude', tde.Type.DOUBLE)
+tableDef.addColumn('latitude', tde.Type.DOUBLE)
+tableDef.addColumn('country_code', tde.Type.UNICODE_STRING)
+tableDef.addColumn('place_type', tde.Type.UNICODE_STRING)
+tableDef.addColumn('full_name', tde.Type.UNICODE_STRING)
+
+#Step 3: Create the table in the image of tableDef
+table = tdefile.addTable('Extract', tableDef)
+
+
+
+
+#Right below we authenticate ourselves
+#You have to enter the credentials from your own Twitter dev. account.
+#This will be found after signing into your dev.twitter.com account
+#Go to My Applications >> OAuth tool to find them
+consumerkey = ''
+consumersecret = ''
+authtoken = ''
+authsecret = ''
+
+api = TwitterAPI(consumerkey,consumersecret,authtoken,authsecret)
+
+r = api.request('statuses/sample', {})
+
+i = 0
+p = 0
+c = 0
+
+#Set the number of Tweets you want
+TWEETS_TO_GET = 10000
+
+t = time.time()
+for item in r.get_iterator():
+    try:
+        newrow = tde.Row(tableDef)
+        newrow.setString(0,item['user']['screen_name'])
+        newrow.setString(1,item['created_at'])
+        newrow.setCharString(2,str(item['id']))
+        newrow.setString(3,item['text'])
+        newrow.setString(4,item['lang'])
+        #newrow.setCharString(5,item['place'])
+        
+        if item['place']: 
+            #print item['place']
+            newrow.setString(5,item['place']['country'])
+            newrow.setString(8,item['place']['country_code'])
+            newrow.setString(9,item['place']['place_type'])
+            newrow.setString(10,item['place']['full_name'])
+            p += 1
+        
+        if item['coordinates']:
+            newrow.setDouble(6,item['coordinates']['coordinates'][0])
+            newrow.setDouble(7,item['coordinates']['coordinates'][1])
+            c += 1
+        
+        
+        table.insert(newrow)
+        i += 1
+        print i
+        if not i % 10: print i
+        if i == TWEETS_TO_GET: 
+            tdefile.close()
+            break
+        print item
+    except:
+        continue
+
+print time.time() - t, p, c
+#tdefile.close()
